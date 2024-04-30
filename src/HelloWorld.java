d26c84258cb978c3acd92e4fd122bdd6407a0539
VitaAeterna <muhleon18@gmail.com>
2024-04-30 17:38:24 +0200

    created files

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+// Leave Space for package and library imports
+
+// Create a class named HelloWorld
+public class HelloWorld{
+    //Create main Method that gets called when Program starts
+    public static void main(String[] args){  // String array args for launch commands
+        System.out.println("Hello World");
+    }
+
+}
